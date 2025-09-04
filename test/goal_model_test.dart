0c22e236c60205480a88ea3964eac0f34aa17252
Ian Darwin <ian@darwinsys.com>
2025-09-04 14:17:09 -0400

    Re-install test script

---
+++ test/goal_model_test.dart
@@ -0,0 +1,18 @@
+import 'package:step_on_it/goal_model.dart';
+import 'package:test/test.dart';
+
+  void main() {
+    test('Setting goal value works', () {
+      final goal = GoalModel();
+      print("Initial goal setting ${goal.goal}");
+      final newGoal = 7500;
+      bool called = false;
+      goal.addListener(() {
+        print("kilroy was here");
+        called = true;
+      });
+      goal.setGoal(newGoal, true);
+      expect(called, true);
+      expect(goal.goal, newGoal);
+    });
+  }
