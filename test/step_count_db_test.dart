3fb749f2665d5d9f919e9066d2d7a36fc2d3dbf7
Ian Darwin <ian@darwinsys.com>
2025-09-09 11:10:20 -0400

    Test 'setCount' (aka update) method; sketch more tests

--- test/step_count_db_test.dart
+++ test/step_count_db_test.dart
@@ -10,13 +10,12 @@ void main() async {
     // Initialize FFI
     sqfliteFfiInit();
     // Change the default factory for unit testing calls for SQFlite
-    databaseFactory = databaseFactoryFfi;
+    databaseFactory = databaseFactoryFfi; // do not inline!
     StepCountDB.database = await databaseFactory.openDatabase(inMemoryDatabasePath);
     db = StepCountDB();
     await db.createTables();
 
     Date now = Date.today();
-    print("Now = $now");
 
     test('find by day test', () async {
       await StepCountDB.database.insert(db.tableName, {'date':  now.toString(), 'count': 42});
@@ -27,5 +26,24 @@ void main() async {
       expect(42, dateCountFromDB.count);
     });
 
+  test("update date count", () async {
+    Date then = Date(2028,11,07);
+    await StepCountDB.database.insert(db.tableName, {'date':  then.toString(), 'count': 42});
+    var dateCountFromDB = await db.findByDate(then);
+    await db.setCount(then, dateCountFromDB.count+1);
+    var dateCount2FromDB = await db.findByDate(then);
+    expect(43, dateCount2FromDB.count);
+  });
 
+  test("find not found returns null", () async {
+    expect(false, await db.existsForDate(Date.fromString("2014-06-10")));
+  });
+
+  test('setCount where already exists', () async {
+    print("test not written yet!");
+  });
+
+  test('setCount where does not exist', () async {
+    print("test not written yet!");
+  });
 }
