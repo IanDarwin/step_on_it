980f096bb88ca36ad6a804c4e970ce56a97e1d29
Ian Darwin <ian@darwinsys.com>
2025-09-09 09:01:06 -0400

    inline the HTML widget

--- lib/nav_drawer.dart
+++ lib/nav_drawer.dart
@@ -3,7 +3,7 @@ import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:in_app_review/in_app_review.dart';
 
-import 'package:step_on_it/main.dart' show goal, version, buildNumber;
+import 'package:step_on_it/main.dart' show version, buildNumber;
 import 'package:step_on_it/settings.dart';
 
 import 'constants.dart';
@@ -14,28 +14,6 @@ const deviceDescription = "Mobile";
 class NavDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var aboutBoxChildren = [
-      HtmlWidget("""
-<html lang="en">
-<h3>You're about to Step On It</h3>
-<p><b>Step On It</b> - The easy way to <em>get up to speed</em> tracking your daily steps
-as you work towards your exercise steps goal today!
-</p><p>
-This app is <em>soooo simple</em> we've not felt obliged to offer a training video!
-</p>
-<p>Built by Ian Darwin of Rejminet Group Inc.
-We stand ready to build all-platform apps for you too.
-Contact ian@darwinsys.com.
-</p>
-<p>Version $version Build $buildNumber<br/>Running on $deviceDescription</p>
-</html>
-      """,
-        onTapUrl: (url) {
-          print("Tapped $url");
-          return true;
-        },
-      ),
-    ];
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
@@ -138,7 +116,28 @@ Contact ian@darwinsys.com.
             applicationVersion: 'September, 2025',
             applicationLegalese:
               '\u{a9} 2025 Rejminet Group Inc.',
-            aboutBoxChildren: aboutBoxChildren,
+                  aboutBoxChildren: [
+                  HtmlWidget("""
+            <html lang="en">
+            <h3>You're about to Step On It</h3>
+            <p><b>Step On It</b> - The easy way to <em>get up to speed</em> tracking your daily steps
+            as you work towards your exercise steps goal today!
+            </p><p>
+            This app is <em>soooo simple</em> we've not felt obliged to offer a training video!
+            </p>
+            <p>Built by Ian Darwin of Rejminet Group Inc.
+            We stand ready to build all-platform apps for you too.
+            Contact ian@darwinsys.com.
+            </p>
+            <p>Version $version Build $buildNumber<br/>Running on $deviceDescription</p>
+            </html>
+            """,
+              onTapUrl: (url) {
+                print("Tapped $url");
+                return true;
+              },
+            ),
+          ],
           ),
         ],
       ),
